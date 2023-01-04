aa2af89f3d29227f22eac8bf21b0834bf26ee721
조혜륜 <absolutecool18@gmail.com>
2023-01-04 21:53:32 +0900

    Create 15656-N과 M(7).py
    - https://www.acmicpc.net/problem/15656
    - back tracking

---
+++ BaekJoon Online Judge/Silver/15656-N과 M(7).py
@@ -0,0 +1,22 @@
+import sys
+
+# 입력 받기
+N, M = map(int, sys.stdin.readline().split())
+numbers = sorted(list(map(int, sys.stdin.readline().split())))
+
+# 수열
+result = []
+
+# back tracking
+def back_tracking():
+    if len(result) == M:
+        print(*result, sep=' ')
+        return
+
+    for i in range(N):
+        result.append(numbers[i])
+        back_tracking()
+        result.pop()
+
+
+back_tracking()
